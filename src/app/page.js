1cd6552633cd7962e15dcaf3b7a2ec7dcdf0b71f
Pranti <prantibanda@gmail.com>
2023-07-01 21:05:16 +0530

    Created basic layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,32 @@
 export default function Home() {
   return (
-    <div className="text-9xl text-red-600">
-      hello
-    </div>
+  <>
+    <body>
+      <nav className="bg-violet-600">
+        <ul className="flex ">
+          <img src="" alt="" />
+          <h1 className="m-4 pr-6 text-white text-xl font-bold">TextUtils</h1>
+          <li className="m-4 text-white hover: cursor-pointer hover:text-purple-400">Home</li>
+          <li className="m-4 text-white hover: cursor-pointer hover:text-purple-400">About</li>
+        </ul>
+      </nav>
+      <div className="pl-[400px] mt-8">
+        
+        <div><h1 className="font-serif text-4xl text-violet-800">Enter the text to analyze:</h1></div>
+        <div>
+        <form className="mt-4">
+          <textarea name="text" id="text" cols="75" rows="6" placeholder="Enter any text..." type="text" className="text-lg placeholder:italic " ></textarea>
+        </form>
+        </div>
+        <div className="mt-3">
+          <button className="p-4 mr-3 rounded-3xl border-violet-900 text-white bg-violet-800 hover:cursor-pointer hover:bg-violet-700">Convert to Uppercase</button>
+          <button className="p-4 mr-3 rounded-3xl border-violet-900 text-white bg-violet-800 hover:cursor-pointer hover:bg-violet-700">Convert to Lowercase</button>
+          <button className="p-4 mr-3 rounded-3xl border-violet-900 text-white bg-violet-800 hover:cursor-pointer hover:bg-violet-700">Copy Text</button>
+          <button className="p-4 mr-3 rounded-3xl border-violet-900 text-white bg-violet-800 hover:cursor-pointer hover:bg-violet-700">Clear Text</button>
+        </div>
+      </div>  
+    </body>
+  </>
+    
   )
 }
